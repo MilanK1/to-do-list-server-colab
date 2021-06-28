7390f3af9dbfc5ff714a908a7e47f121e6deb01e
MilanK1 <54515638+MilanK1@users.noreply.github.com>
2021-06-28 16:29:00 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import home from "./home/home.js";
+import routes from "./modules/core/routes.js";
+
+const app = express();
+const PORT = 8000;
+
+
+routes(app);
+
+app.listen(PORT, () => {
+});
+//app.listen(PORT, () => console.log(`Listening on port http://localhost:${PORT}/`));
